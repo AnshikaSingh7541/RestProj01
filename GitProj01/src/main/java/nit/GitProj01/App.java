921e1d27c40ead5c366c80fe542d65a3cc029b18
AnshikaSingh <anshikasingh91996.singh@example.com>
2025-04-28 20:49:15 +0530

    commit by dev

--- GitProj01/src/main/java/nit/GitProj01/App.java
+++ GitProj01/src/main/java/nit/GitProj01/App.java
@@ -4,7 +4,14 @@ package nit.GitProj01;
  * Hello world!
  */
 public class App {
+	
+	public int sum(int x,int y)
+	{
+		return x+y;
+	}
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        App app=new App();
+        System.out.println("Sum of the no are:-"+app.sum(10,20));
     }
 }
--- GitProj01/src/test/java/nit/GitProj01/AppTest.java
+++ GitProj01/src/test/java/nit/GitProj01/AppTest.java
@@ -1,19 +1,30 @@
 package nit.GitProj01;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest {
 
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void testSumwithPositive() {
+        
+        App app=new App();
+        int expected=300;
+        int actual=app.sum(100, 200);
+        assertEquals(expected, actual);
+        
     }
+    @Test
+    public void testSumwithNegitive() {
+        
+        App app=new App();
+        int expected=100;
+        int actual=app.sum(-100, 200);
+        assertEquals(expected, actual);
+        
+    }
+    
 }
